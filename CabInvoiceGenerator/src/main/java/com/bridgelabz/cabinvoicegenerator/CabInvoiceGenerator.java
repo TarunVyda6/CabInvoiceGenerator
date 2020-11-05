d2960bd28da61a00037a7a3f4915f2057ba07223
TarunVyda6 <kamaltarun.rao6@gmail.com>
2020-11-05 21:24:13 +0530

    Added : Feature of getting invoice for multiple rides

--- CabInvoiceGenerator/src/main/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGenerator.java
+++ CabInvoiceGenerator/src/main/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGenerator.java
@@ -13,14 +13,14 @@ public class CabInvoiceGenerator
 		return Math.max(totalFare, MINIMUM_FARE);
 	}
 
-	public double calculateFare(Ride[] rides)
+	public InvoiceSummary calculateFare(Ride[] rides)
 	{
 		double totalFare = 0;
 		for (Ride ride : rides)
 		{
 			totalFare += calculateFare(ride.distance, ride.time);
 		}
-		return totalFare;
+		return new InvoiceSummary(rides.length, totalFare);
 	}
 
 }
--- CabInvoiceGenerator/src/test/java/com/bridgelabz/cabinvoicegenerator/InvoiceServiceTest.java
+++ CabInvoiceGenerator/src/test/java/com/bridgelabz/cabinvoicegenerator/InvoiceServiceTest.java
@@ -5,10 +5,11 @@ import org.junit.Test;
 
 public class InvoiceServiceTest
 {
+	CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+
 	@Test
 	public void givenDistanceAndTime_WhenAnalyse_ShouldReturnTotalFare()
 	{
-		CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
 		double totalFare = cabInvoiceGenerator.calculateFare(2.5, 5);
 		Assert.assertEquals(30, totalFare, 0.0);
 	}
@@ -16,21 +17,18 @@ public class InvoiceServiceTest
 	@Test
 	public void givenLessDistanceAndTime_WhenAnalyse_ShouldReturnMinimumFare()
 	{
-		CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
 		double totalFare = cabInvoiceGenerator.calculateFare(0.1, 1);
 		Assert.assertEquals(5, totalFare, 0.0);
 	}
 
 	@Test
-	public void givenMultipleRides_WhenAnalyse_ShouldReturnAggregate()
+	public void givenMultipleRides_WhenAnalyse_ShouldReturnInvoiceSummary()
 	{
-		CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+
 		Ride[] rides = { new Ride(2.5, 5), new Ride(0.2, 1) };
-		double fare = cabInvoiceGenerator.calculateFare(rides);
-		Assert.assertEquals(35, fare, 0);
-		
-		
+		InvoiceSummary actualInvoiceSummary = cabInvoiceGenerator.calculateFare(rides);
+		InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 35);
+		Assert.assertEquals(expectedInvoiceSummary, actualInvoiceSummary);
 	}
-	
-	
+
 }
